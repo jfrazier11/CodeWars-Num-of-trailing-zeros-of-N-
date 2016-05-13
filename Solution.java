b886623c0bb53441a39ba9b5e24dd7ad3cef4d0a
Jeremi Frazier <jerm_803@yahoo.com>
2016-05-13 15:45:44 -0400

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,6 +1,16 @@
 import java.lang.Math;
+/**
+ * This class is a solution to the infamous number of trailing zeros problem
+ * @author Jeremi Frazier
+ * @version 1.0
+ */
 public class Solution {
-
+  /**
+   * this method takes an integer input  and determines the number of trailing zeroes.
+   * the solution is made to withstand even 1000! factorial!
+   * @param n -the number
+   * @return int -the number of trailing zeroes
+   */
   public static int zeros(int n) {
   int numOfZeroes=0;
   while(n!= 0){
